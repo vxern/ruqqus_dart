ca04942170e816a4c75d60b7fb744eb85a6f4e98
devongalat <devongalat@gmail.com>
2020-09-24 18:22:31 +0100

    Update

--- lib/src/client.dart
+++ lib/src/client.dart
@@ -6,39 +6,64 @@ import 'package:dio/dio.dart';
 import 'logger.dart';
 
 class Client extends EventEmitter {
-  static const String grant_url = 'https://ruqqus.com/oauth/grant';
+  static const String website_link = "https://ruqqus.com";
+  static const String grant_url = '$website_link/grant';
 
-  Dio dio;
-  Map<String, String> fetchKeys;
-  final client_id;
-  final client_secret;
-  final code;
-  final user_agent;
+  Dio dio = Dio();
 
+  Map<String, dynamic> requestData;
+  Map<String, bool> scopes;
+  DateTime nextRefresh;
   bool isOnline;
 
-  Client(this.client_id, this.client_secret, this.code, this.user_agent) {
-    dio = Dio();
-
-    fetchKeys = {
+  Client(client_id, String client_secret, String code, String user_agent) {
+    requestData = {
       'client_id': client_id,
       'client_secret': client_secret,
       'grant_type': 'code',
-      'code': code ?? null
+      'code': code ?? null,
+      'user_agent': user_agent
     };
+
+    () async => await _getToken();
   }
 
   /// Ruqqus: "Access tokens expire one hour after they are issued.
   /// To maintain ongoing access, you will need to use the refresh token to obtain a new access token."
-  void getToken() async {
-    Response response = await dio.post(grant_url,
-        data: fetchKeys, options: Options(headers: {'User-Agent': user_agent}));
+  void _getToken() async {
+    Response response;
 
-    // If the request is not '200 - OK'
+    // Make a ruqquest to Ruqqus
+    response = await dio.post(grant_url,
+        data: FormData.fromMap(requestData),
+        options: Options(headers: {
+          'User-Agent': requestData['user_agent'],
+          'X-Poster-Type': 'bot'
+        }));
+
+    // If not successful, print the error.
     if (!(response.statusCode >= 200 && response.statusCode <= 299)) {
       throwError('${response.statusCode} - ${response.statusMessage}');
       sleep(Duration(seconds: 10));
       exit;
+    }
+
+    log(Severity.Debug, response.data.toString());
+    // Build next ruqquest
+    requestData['access_token'] = response.data['access_token'];
+    requestData['refresh_token'] = response.data['refresh_token'];
+    nextRefresh = DateTime.parse(response.data[
+        'expires_at']); // I'll work out how to set the time back by 5 seconds later
+
+    // Set up client
+    if (requestData['grant_type'] == 'code') {
+      requestData['grant_type'] = 'refresh';
+
+      // Enable the scopes defined in the response
+      response.data.forEach((scope) => {scopes['scope'] = true});
+
+      isOnline = true;
+      emit('ready');
     }
   }
 }
