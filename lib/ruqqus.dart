24c278e7b740cc250c36c71691a29c03a5c7c8cf
Devon Galatin <56321034+devongalat@users.noreply.github.com>
2020-09-28 14:03:45 +0100

    Update ruqqus.dart

--- lib/ruqqus.dart
+++ lib/ruqqus.dart
@@ -3,5 +3,6 @@ library ruqqus;
 export 'src/structs/primary.dart';
 export 'src/structs/submissions.dart';
 export 'src/structs/users.dart';
+export 'src/logger.dart';
 export 'src/client.dart';
 export 'src/API.dart';
