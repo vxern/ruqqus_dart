8f9ddbb3c4f0d1f7cd79e8952af8cbb47248da72
devongalat <devongalat@gmail.com>
2020-09-29 11:10:20 +0100

    Update ruqqus.dart

--- lib/ruqqus.dart
+++ lib/ruqqus.dart
@@ -1,6 +1,8 @@
 library ruqqus;
 
+export 'src/structs/guilds.dart';
 export 'src/structs/primary.dart';
+export 'src/structs/settings.dart';
 export 'src/structs/submissions.dart';
 export 'src/structs/users.dart';
 export 'src/logger.dart';
