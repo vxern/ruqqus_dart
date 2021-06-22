3b4984f6787fb62b7e93a0685520b9d995508560
Dorian Oszczęda <vxern@wordcollector.co.uk>
2021-06-23 00:02:47 +0100

    Updated and revised `settings.dart`

--- lib/src/structs/settings.dart
+++ lib/src/structs/settings.dart
@@ -1,65 +1,58 @@
-// structs/settings.dart - File for storing classes used for various settings
-
-/// Your profile settings
 class ProfileSettings {
-  bool over_18;
-  bool hide_offensive;
-  bool show_nsfl;
-  bool filter_nsfw;
-  bool private;
-  bool nofollow;
-  String bio;
-  String title_id;
+  final bool over18;
+  final bool hideOffensive;
+  final bool showNsfl;
+  final bool filterNsfw;
+  final bool private;
+  final bool nofollow;
+  final String bio;
+  final String titleId;
 
-  ProfileSettings(
-      {this.over_18,
-      this.hide_offensive,
-      this.show_nsfl,
-      this.filter_nsfw,
-      this.private,
-      this.nofollow,
-      this.bio,
-      this.title_id});
+  const ProfileSettings(
+    this.over18,
+    this.hideOffensive,
+    this.showNsfl,
+    this.filterNsfw,
+    this.private,
+    this.nofollow,
+    this.bio,
+    this.titleId,
+  );
 }
 
-/// Struct for new password
 class UpdatePassword {
-  final String new_password;
-  final String old_password;
+  final String newPassword;
+  final String oldPassword;
 
-  UpdatePassword({this.new_password, this.old_password});
+  const UpdatePassword(this.newPassword, this.oldPassword);
 }
 
-/// Struct for new email
 class UpdateEmail {
-  final String new_email;
+  final String newEmail;
   final String password;
 
-  UpdateEmail({this.new_email, this.password});
+  const UpdateEmail(this.newEmail, this.password);
 }
 
-/// Struct for 2fa enabling
 class Enable2FA {
-  final String two_factor_secret;
-  final String two_factor_token;
+  final String twoFactorSecret;
+  final String twoFactorToken;
   final String password;
 
-  Enable2FA({this.two_factor_secret, this.two_factor_token, this.password});
+  const Enable2FA(this.twoFactorSecret, this.twoFactorToken, this.password);
 }
 
-/// Struct for 2fa disabling
 class Disable2FA {
-  final String two_factor_token;
+  final String twoFactorToken;
   final String password;
 
-  Disable2FA({this.two_factor_token, this.password});
+  const Disable2FA(this.twoFactorToken, this.password);
 }
 
-/// Struct for deleting account
 class AccountDeletion {
   final String password;
-  String delete_reason;
-  String two_factor_token;
+  String? deleteReason;
+  String? twoFactorToken;
 
-  AccountDeletion(this.password, {this.delete_reason, this.two_factor_token});
+  AccountDeletion(this.password, [this.deleteReason, this.twoFactorToken]);
 }
