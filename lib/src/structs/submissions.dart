a78d337e11f74df92757d36988161df22f018822
Dorian Oszczęda <vxern@wordcollector.co.uk>
2021-06-23 00:02:37 +0100

    Updated and revised `submissions.dart`

--- lib/src/structs/submissions.dart
+++ lib/src/structs/submissions.dart
@@ -1,191 +1,276 @@
-// structs/submissions.dart - File for storing classes used for post / comment data
+import 'dart:convert';
 
-import 'package:dio/dio.dart';
+import 'package:ruqqus_dart/src/API.dart';
+import 'package:ruqqus_dart/src/structs/primary.dart';
+import 'package:ruqqus_dart/src/structs/users.dart';
 
-import '../API.dart';
-import '../logger.dart';
-import 'users.dart';
-import 'guilds.dart';
-import 'primary.dart';
-
-/// The post class. Comprises info about a post
 class Post extends Primary {
-  API api;
+  final API api;
 
-  Author author;
-  Content content;
-  Votes votes;
-  int edited_at;
-  SubmissionFlags flags;
-  GuildName guild_name;
+  SubmissionAuthor? author;
+  SubmissionContent? content;
+  SubmissionVotes? votes;
+  SubmissionFlags? flags;
+  GuildName? guildName;
+  int? editedAt;
 
-  Post(API api);
+  Post(this.api);
 
-  void obtainData(String id, Map<String, dynamic> suppliedData) async {
-    Response response;
+  factory Post.from(API api, Map<String, dynamic> data) {
+    print(data);
 
-    // If we already have the data for which a get request would have been otherwise needed, use that
-    if (suppliedData != null) {
-      response = Response(data: suppliedData);
-    } else {
-      response = await api.GetRequest('post/$id', headers: {'sort': 'top'});
-    }
+    final post = Post(api);
 
-    if (response.data['id'] == null) {
-      throwError('Could not obtain id of post!');
+    // Primary data
+    post.id = data['id'];
+    post.fullId = data['fullname'];
+    post.link = data['permalink'];
+    post.fullLink = '${API.host}${post.link}';
+    post.createdAt = data['created_utc'];
+
+    // Post-specific data
+    post.author = SubmissionAuthor(
+      data['author_name'],
+      data.containsKey('author_title')
+          ? Title.from(data['author_title'])
+          : null,
+    );
+    post.content = SubmissionContent(
+      data['title'],
+      Body(data['body'], data['body_html']),
+      data['domain'],
+      data['url'],
+      data['thumb_url'],
+      data['embed_url'],
+    );
+    post.votes = SubmissionVotes(
+      data['score'],
+      data['upvotes'],
+      data['downvotes'],
+      data['voted'],
+    );
+    post.flags = SubmissionFlags(
+      data['is_archived'],
+      data['is_banned'],
+      data['is_deleted'],
+      data['is_nsfw'],
+      data['is_nsfl'],
+      data['is_offensive'],
+    );
+    post.guildName = GuildName(data['guild_name'], data['original_guild_name']);
+    post.editedAt = data['edited_utc'];
+
+    return post;
+  }
+
+  Future fetchData(String id) async {
+    final response = await api.get('/post/$id');
+
+    if (response == null) {
+      api.log.error('Failed to fetch data of post $id');
       return;
     }
 
-    this.id = response.data['id'];
-    full_id = response.data['fullname'];
-    link = response.data['permalink'];
-    full_link = '$API.website_link$link';
-    created_at = response.data['created_utc'];
-    edited_at = response.data['edited_utc'];
+    final Map<String, dynamic> data = jsonDecode(response.body);
 
+    // Primary data
+    id = data['id'];
+    fullId = data['fullname'];
+    link = data['permalink'];
+    fullLink = '${API.host}$link';
+    createdAt = data['created_utc'];
+
+    // Post-specific data
+    author = SubmissionAuthor(
+      data['author_name'],
+      data.containsKey('author_title')
+          ? Title.from(data['author_title'])
+          : null,
+    );
+    content = SubmissionContent(
+      data['title'],
+      Body(data['body'], data['body_html']),
+      data['domain'],
+      data['url'],
+      data['thumb_url'],
+      data['embed_url'],
+    );
+    votes = SubmissionVotes(
+      data['score'],
+      data['upvotes'],
+      data['downvotes'],
+      data['voted'],
+    );
     flags = SubmissionFlags(
-        response.data['is_archived'] == '1' ? true : false,
-        response.data['is_banned'] == '1' ? true : false,
-        response.data['is_deleted'] == '1' ? true : false,
-        response.data['is_nsfw'] == '1' ? true : false,
-        response.data['is_nsfl'] == '1' ? true : false,
-        response.data['is_offensive'] == '1' ? true : false,
-        edited_at > 0);
-
-    author = Author(
-        response.data['author'],
-        response.data['author_title'] != null
-            ? Title(
-                response.data['author_title']['text'].startsWith(',')
-                    ? response.data['author_title']['text'].split(', ')[1]
-                    : response.data['author_title']['text'],
-                response.data['author_title']['id'].toString(),
-                response.data['author_title']['kind'].toString(),
-                response.data['author_title']['color'].toString())
-            : null);
-
-    content = Content(
-        response.data['title'],
-        Body(response.data['body'], response.data['body_html']),
-        response.data['domain'],
-        response.data['url'],
-        response.data['thumb_url'],
-        response.data['embed_url']);
-
-    votes = Votes(response.data['score'], response.data['upvotes'],
-        response.data['downvotes'], response.data['voted']);
-
-    guild_name = GuildName(
-        response.data['guild_name'], response.data['original_guild_name']);
+      data['is_archived'],
+      data['is_banned'],
+      data['is_deleted'],
+      data['is_nsfw'],
+      data['is_nsfl'],
+      data['is_offensive'],
+    );
+    guildName = GuildName(data['guild_name'], data['original_guild_name']);
+    editedAt = data['edited_utc'];
   }
 }
 
 class Comment extends Primary {
-  API api;
+  final API api;
 
-  Author author;
-  Body body;
-  Votes votes;
-  int edited_at;
-  SubmissionFlags flags;
-  GuildName guild_name;
+  SubmissionAuthor? author;
+  Body? body;
+  SubmissionVotes? votes;
+  SubmissionFlags? flags;
+  GuildName? guildName;
+  int? editedAt;
 
-  Comment(API api);
+  Comment(this.api);
 
-  void obtainData(String id, Map<String, dynamic> suppliedData) async {
-    Response response;
+  factory Comment.from(API api, Map<String, dynamic> data) {
+    final comment = Comment(api);
 
-    // If we already have the data for which a get request would have been otherwise needed, use that
-    if (suppliedData != null) {
-      response = Response(data: suppliedData);
-    } else {
-      response = await api.GetRequest('comment/$id', headers: {'sort': 'top'});
-    }
+    // Primary data
+    comment.id = data['id'];
+    comment.fullId = data['fullname'];
+    comment.link = data['permalink'];
+    comment.fullLink = '${API.host}${comment.link}';
+    comment.createdAt = data['created_utc'];
 
-    if (response.data['id'] == null) {
-      throwError('Could not obtain id of comment!');
+    // Comment-specific data
+    comment.author = SubmissionAuthor(
+      data['author_name'],
+      data.containsKey('author_title')
+          ? Title.from(data['author_title'])
+          : null,
+    );
+    comment.body = Body(data['body'], data['body_html']);
+    comment.votes = SubmissionVotes(
+      data['score'],
+      data['upvotes'],
+      data['downvotes'],
+      data['voted'],
+    );
+    comment.flags = SubmissionFlags(
+      data['is_archived'],
+      data['is_banned'],
+      data['is_deleted'],
+      data['is_nsfw'],
+      data['is_nsfl'],
+      data['is_offensive'],
+    );
+    comment.guildName = GuildName(
+      data['post']['guild_name'],
+      data['post']['original_guild_name'],
+    );
+    comment.editedAt = data['edited_utc'];
+
+    return comment;
+  }
+
+  Future fetchData(String id) async {
+    final response = await api.get('/comment/$id');
+
+    if (response == null) {
+      api.log.error('Failed to fetch data of comment $id');
       return;
     }
 
-    this.id = response.data['id'];
-    full_id = response.data['fullname'];
-    link = response.data['permalink'];
-    full_link = '$API.website_link$link';
-    created_at = response.data['created_utc'];
-    edited_at = response.data['edited_utc'];
+    final Map<String, dynamic> data = jsonDecode(response.body);
 
+    // Primary data
+    id = data['id'];
+    fullId = data['fullname'];
+    link = data['permalink'];
+    fullLink = '${API.host}$link';
+    createdAt = data['created_utc'];
+
+    // Post-specific data
+    author = SubmissionAuthor(
+      data['author_name'],
+      data.containsKey('author_title')
+          ? Title.from(data['author_title'])
+          : null,
+    );
+    body = Body(data['body'], data['body_html']);
+    votes = SubmissionVotes(
+      data['score'],
+      data['upvotes'],
+      data['downvotes'],
+      data['voted'],
+    );
     flags = SubmissionFlags(
-        response.data['is_archived'] == '1' ? true : false,
-        response.data['is_banned'] == '1' ? true : false,
-        response.data['is_deleted'] == '1' ? true : false,
-        response.data['is_nsfw'] == '1' ? true : false,
-        response.data['is_nsfl'] == '1' ? true : false,
-        response.data['is_offensive'] == '1' ? true : false,
-        edited_at > 0);
-
-    author = Author(
-        response.data['author'],
-        response.data['title'] != null
-            ? Title(
-                response.data['title']['text'].startsWith(',')
-                    ? response.data['title']['text'].split(', ')[1]
-                    : response.data['title']['text'],
-                response.data['title']['id'].toString(),
-                response.data['title']['kind'].toString(),
-                response.data['title']['color'].toString())
-            : null);
-
-    body = Body(response.data['body'], response.data['body_html']);
-
-    votes = Votes(response.data['score'], response.data['upvotes'],
-        response.data['downvotes'], response.data['score']);
-
-    guild_name =
-        GuildName(response.data['guild_name'], response.data['guild_name']);
+      data['is_archived'],
+      data['is_banned'],
+      data['is_deleted'],
+      data['is_nsfw'],
+      data['is_nsfl'],
+      data['is_offensive'],
+    );
+    guildName = GuildName(
+      data['post']['guild_name'],
+      data['post']['original_guild_name'],
+    );
+    editedAt = data['edited_utc'];
   }
 }
 
-/// The post flags
 class SubmissionFlags {
-  final bool is_archived;
-  final bool is_banned;
-  final bool is_deleted;
-  final bool is_nsfw;
-  final bool is_nsfl;
-  final bool is_offensive;
-  final bool is_edited;
+  final bool isArchived;
+  final bool isBanned;
+  final bool isDeleted;
+  final bool isNsfw;
+  final bool isNsfl;
+  final bool isOffensive;
 
-  SubmissionFlags(this.is_archived, this.is_banned, this.is_deleted,
-      this.is_nsfw, this.is_nsfl, this.is_offensive, this.is_edited);
+  const SubmissionFlags(
+    this.isArchived,
+    this.isBanned,
+    this.isDeleted,
+    this.isNsfw,
+    this.isNsfl,
+    this.isOffensive,
+  );
 }
 
-/// The content of the post
-class Content {
+class SubmissionContent {
   final String title;
   final Body body;
   final String domain;
   final String url;
-  final String thumb_url;
-  final String embed_url;
+  final String? thumbUrl;
+  final String? embedUrl;
 
-  Content(this.title, this.body, this.domain, this.url, this.thumb_url,
-      this.embed_url);
+  const SubmissionContent(
+    this.title,
+    this.body,
+    this.domain,
+    this.url,
+    this.thumbUrl,
+    this.embedUrl,
+  );
 }
 
-/// The votes of a submission
-class Votes {
+class SubmissionVotes {
   final int score;
   final int upvotes;
   final int downvotes;
-  final int voted;
+  final int? voted;
 
-  Votes(this.score, this.upvotes, this.downvotes, this.voted);
+  const SubmissionVotes(this.score, this.upvotes, this.downvotes, this.voted);
 }
 
-/// The author of the post / comment
-class Author {
+class SubmissionAuthor {
   final String username;
-  final Title title;
+  Title? title;
 
-  Author(this.username, this.title);
+  SubmissionAuthor(this.username, [this.title]);
 }
+
+class GuildName {
+  final String guildName;
+  final String? originalGuildName;
+
+  const GuildName(this.guildName, this.originalGuildName);
+}
+
+enum SubmissionType { Post, Comment }
