69771d29b55c57898e3a77cbb0cd99fb45e2759e
Dorian Oszczęda <vxern@wordcollector.co.uk>
2021-06-23 00:01:57 +0100

    Organised exports

--- lib/ruqqus_dart.dart
+++ lib/ruqqus_dart.dart
@@ -1,10 +1,12 @@
 library ruqqus_dart;
 
+// Interfaces
+export 'src/API.dart';
+export 'src/client.dart';
+
+// Data structures
 export 'src/structs/guilds.dart';
 export 'src/structs/primary.dart';
 export 'src/structs/settings.dart';
 export 'src/structs/submissions.dart';
 export 'src/structs/users.dart';
-export 'src/logger.dart';
-export 'src/client.dart';
-export 'src/API.dart';
