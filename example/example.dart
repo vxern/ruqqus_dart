e5f7e6c5ebffed98fe924b2a079946878d4ae523
Dorian Oszczęda <vxern@wordcollector.co.uk>
2021-06-24 19:00:01 +0100

    Created `example.dart`

---
+++ example/example.dart
@@ -0,0 +1,33 @@
+import 'package:ruqqus_dart/ruqqus_dart.dart';
+import 'package:ruqqus_dart/src/utils.dart';
+
+void main() async {
+  final client = Client(
+    clientId: 'client_id',
+    clientSecret: 'client_secret',
+    refreshToken: 'refresh_token',
+    userAgent: 'ruqqus.dart by @vxern',
+  );
+
+  client.on('ready', () async {
+    final post = await client.api.submitPost(
+      targetGuild: 'FormatPlayground',
+      title: generateRandomString(),
+      body: 'Title ^',
+    );
+
+    final comment = await client.api.submitReply(
+      parentSubmissionType: SubmissionType.Post,
+      id: post!.id!,
+      body: generateRandomString(length: 1000, addSpaces: true),
+    );
+
+    await client.api.submitReply(
+      parentSubmissionType: SubmissionType.Comment,
+      id: comment!.id!,
+      body: generateRandomString(length: 1000, addSpaces: true),
+    );
+  });
+
+  client.login();
+}
